7ecc8122cb8b0c60fc6880aa9c9ad849b82d347e
zondiMaqina <zondimaqina0@gmail.com>
2025-02-13 13:20:53 +0200

    test methods
    methods that let player choose color to play with in the game

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -5,8 +5,7 @@ require_relative '../lib/players_data/players'
 
 RSpec.describe Players do
   # #initialze => assigns objects to vars
-  # #receive_players_data => Public Script method
-
+  # #show_colors => only prints
   subject(:player_name) { described_class.new }
 
   describe '#receive_player1_name' do
@@ -32,6 +31,52 @@ RSpec.describe Players do
     it "will store player's name" do
       saved_name = 'player2'
       expect{ player_name.receive_player1_name }.to change(player_name, :game_players).from([]).to([saved_name])
+    end
+  end
+
+  subject(:player_color) { described_class.new }
+
+  describe '#choose_color' do
+    context 'when player chooses their color of choice' do
+      before do
+        chosen_color = 'green'
+        allow(player_color).to receive(:chosen_color).and_return(chosen_color)
+        allow(player_color).to receive(:puts)
+      end
+  
+      it 'will let be saved to array' do
+        expect{ player_color.choose_color }.to change{ player_color.chosen_moves }
+      end
+    end
+
+    context 'when player has chosen color' do
+      before do
+        color = 'green'
+        allow(player_color).to receive(:gets).and_return(color)
+        allow(player_color).to receive(:puts)
+      end
+
+      it 'will be removed from available colors' do
+        chosen_color = 'green'
+        player_color.choose_color
+        expect(player_color.available_colors).not_to include(chosen_color)
+      end
+    end
+  end
+
+  describe '#chosen_color' do
+    context 'when player enters non-existant color' do
+      before do
+        invalid_color = 'gold'
+        valid_color = 'green'
+        allow(player_color).to receive(:gets).and_return(invalid_color, valid_color)
+      end
+
+      it 'will ask player to enter color again' do
+        message = 'Color does not exist or already chosen'
+        expect(player_color).to receive(:puts).with(message)
+        player_color.chosen_color
+      end
     end
   end
 end
