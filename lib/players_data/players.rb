4007f95521c14514f30d6182eab2420007b509e5
zondiMaqina <zondimaqina0@gmail.com>
2025-02-13 13:21:14 +0200

    create methods for letting player choose colos to play with

--- lib/players_data/players.rb
+++ lib/players_data/players.rb
@@ -3,11 +3,10 @@ require 'colorize'
 
 # class for gathering players' data
 class Players
-  attr_reader :game_players
+  attr_reader :game_players, :chosen_moves, :available_colors
 
   def initialize
     @game_players = []
-    @players_colors = []
     @available_colors = %w[red green white orange]
     @chosen_moves = []
   end
@@ -20,5 +19,27 @@ class Players
   def receive_player2_name
     puts 'Enter your name player 2'.bold
     game_players << gets.chomp # player name
+  end
+
+  def choose_color
+    show_colors
+    puts 'Choose your color to play with :)'
+    @chosen_moves << "\u25A0".colorize(chosen_color.to_sym) # returns valid chosen color
+  end
+
+  def show_colors
+    puts <<-HEREDOC
+      The evailable colors to choose from are
+      #{@available_colors}
+    HEREDOC
+  end
+
+  def chosen_color
+    color = gets.chomp
+    until @available_colors.include?(color)
+      puts 'Color does not exist or already chosen'
+      color = gets.chomp
+    end
+    return @available_colors.delete(color)
   end
 end
